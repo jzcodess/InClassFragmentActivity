cd549d370f8aeafacdf346c9e8ae316d13775f41
jzcodess <tuq67092@temple.edu>
2023-03-23 15:09:40 -0400

    3/23/2023

--- app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
+++ app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
@@ -9,24 +9,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.selects.select
 
 const val IMAGES_KEY = "imageList"
 
 class ImageDisplayFragment : Fragment() {
-
-    private lateinit var images: IntArray
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // If we have arguments
-        arguments?.let { it ->
-            // If we find the specific argument
-            it.getIntArray(IMAGES_KEY)?.let {
-                images = it
-            }
-        }
+=
+    private val mainViewModel : MainViewModel by lazy{
+        ViewModelProvider(requireActivity())[MainViewModel::class.java]
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -40,17 +33,18 @@ class ImageDisplayFragment : Fragment() {
         // The recycler view is the root element of the Fragment's layout
         // as such the view argument passed to onViewCreated() is the RecyclerView
         with (view as RecyclerView) {
-            adapter = CustomRecyclerAdapter(images)
+
+            mainViewModel.getImageIds().observe(requireActivity()){
+                adapter = CustomRecyclerAdapter(it){
+                    (requireActivity() as ImageSelectedInterface).imageSelected()
+                }
+            }
+
             layoutManager = GridLayoutManager(requireContext(), 2)
         }
     }
 
-    companion object {
-        fun newInstance(images: IntArray) =
-            ImageDisplayFragment().apply {
-                arguments = Bundle().apply {
-                    putIntArray(IMAGES_KEY, images)
-                }
-            }
+    interface ImageSelectedInterface{
+        fun imageSelected(itemId: Int)
     }
 }
--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,16 +2,21 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
+
+        mainViewModel.setImageIds(imageArray)
 
         val imageArray1 = ImageDisplayFragment.newInstance(imageArray)
 
@@ -21,4 +26,6 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.imageFragmentContainer, imageArray1)
             .commit()
     }
+
+    override fun imageSelected(itemId)
 }
